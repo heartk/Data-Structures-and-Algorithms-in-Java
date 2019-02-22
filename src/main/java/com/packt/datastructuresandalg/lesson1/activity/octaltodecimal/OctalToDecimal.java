ad76bd32da27814b476e809d43337ccc5acbbe8e
rhsund <it_sun@126.com>
2019-02-22 14:47:14 +0800

    添加示例 converBinaryNumbersToDecimal

--- src/main/java/com/packt/datastructuresandalg/lesson1/activity/octaltodecimal/OctalToDecimal.java
+++ src/main/java/com/packt/datastructuresandalg/lesson1/activity/octaltodecimal/OctalToDecimal.java
@@ -4,4 +4,19 @@ public class OctalToDecimal {
     public int convertToDecimal(String octal) {
         return 999;
     }
+
+    public static int convertBinaryNumbersToDecimal(String binary) {
+        int result = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary.charAt(binary.length() - i - 1) == '1') {
+                result += Math.pow(2, i);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertBinaryNumbersToDecimal("10110"));
+    }
+
 }
